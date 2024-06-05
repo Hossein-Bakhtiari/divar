7d0deab59d3a32b399da760c6de04a00780bc60f
hossein <hosseinbakhtiari781@outlook.com>
2024-06-05 21:16:20 +0330

    check mobile and code in checkOtp  and send response from backend

--- src/components/templates/CheckOtpForm.jsx
+++ src/components/templates/CheckOtpForm.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import { toast } from "react-toastify";
+import { checkOtp } from "../../services/auth";
 
-function CheckOtpForm() {
+function CheckOtpForm({ code, setCode, mobile, setStep }) {
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    if (code.length !== 5) {
+      toast.error("کد تایید نامعتبر است");
+      return;
+    }
+
+    const { response, error } = await checkOtp(mobile, code);
+    console.log({response , error})
+  };
   return (
-    <div>CheckOtpForm</div>
-  )
+    <form onSubmit={submitHandler}>
+      <p>تایید کد پیامک شده</p>
+      <span>کد پیامک شده به شماره «{mobile}» را وارد کنید</span>
+      <label htmlFor="input">کد تایید را وارد کنید</label>
+      <input
+        type="text"
+        id="input"
+        placeholder="کد تایید"
+        value={code}
+        onChange={(e) => setCode(e.target.value)}
+        ref={input}
+      />
+      <button type="submit">ورود</button>
+      <button onClick={() => setStep(1)}>تغیر شماره موبایل</button>
+    </form>
+  );
 }
 
-export default CheckOtpForm
+export default CheckOtpForm;
